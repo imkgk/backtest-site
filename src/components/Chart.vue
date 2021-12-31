e68b7d8d7260e856e97913d7cd175de435d32706
imkgk <lnz013@gmail.com>
2021-12-31 19:59:57 +0800

    test a chart

---
+++ src/components/Chart.vue
@@ -0,0 +1,61 @@
+<template>
+  <v-chart class="chart" :option="option" />
+</template>
+
+<script>
+import { use } from "echarts/core";
+import { CanvasRenderer } from "echarts/renderers";
+import { LineChart } from "echarts/charts";
+import {
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent
+} from "echarts/components";
+import VChart, { THEME_KEY } from "vue-echarts";
+import { ref, defineComponent } from "vue";
+
+use([
+  CanvasRenderer,
+  LineChart,
+  LegendComponent,
+  TitleComponent,
+  TooltipComponent,
+  GridComponent
+]);
+
+export default defineComponent({
+  name: "HelloWorld",
+  components: {
+    VChart
+  },
+  provide: {
+    [THEME_KEY]: "dark"
+  },
+  setup() {
+    const option = ref({
+      xAxis: {
+        type: 'category',
+        data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+      },
+      yAxis: {
+        type: 'value'
+      },
+      series: [
+        {
+          data: [150, 230, 224, 218, 135, 147, 260],
+          type: 'line'
+        }
+      ]
+    });
+
+    return { option };
+  }
+});
+</script>
+
+<style scoped>
+.chart {
+  height: 400px;
+}
+</style>
